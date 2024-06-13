b38ca0cecca85a7a0128836922df6f535cf11ac5
Jordan Maynor <mail@jtm.li>
2024-06-13 10:17:51 -0500

    Additional functions

--- ezo.py
+++ ezo.py
@@ -6,8 +6,6 @@ from typing import Optional
 
 import requests
 
-# Members Functions
-
 
 def get_members(filter: Optional[dict]) -> list[dict]:
     """
@@ -123,7 +121,72 @@ def get_member_details(member_id: int) -> dict:
     return response.json()
 
 
-# Locations Functions
+def create_member(member: dict) -> dict:
+    """
+    Create a new member
+    """
+    if "EZO_BASE_URL" not in os.environ:
+        raise Exception("EZO_BASE_URL not found in environment variables.")
+    if "EZO_TOKEN" not in os.environ:
+        raise Exception("EZO_TOKEN not found in environment variables.")
+
+    # Required fields
+    if "user[email]" not in member:
+        raise ValueError("member must have 'user[email]' key")
+    if "user[first_name]" not in member:
+        raise ValueError("member must have 'user[first_name]' key")
+    if "user[last_name]" not in member:
+        raise ValueError("member must have 'user[last_name]' key")
+    if "user[role_id]" not in member:
+        raise ValueError("member must have 'user[role_id]' key")
+
+    # Remove any keys that are not valid
+    valid_keys = [
+        "user[email]",
+        "user[employee_id]",
+        "user[role_id]",
+        "user[team_id]",
+        "user[user_listing_id]",
+        "user[first_name]",
+        "user[last_name]",
+        "user[address_name]",
+        "user[address]",
+        "user[address_line_2]",
+        "user[city]",
+        "user[state]",
+        "user[country]",
+        "user[phone_number]",
+        "user[fax]",
+        "user[login_enabled]",
+        "user[subscribed_to_emails]",
+        "skip_confirmation_email",
+    ]
+
+    # Check for custom attributes
+    member = {
+        k: v
+        for k, v in member.items()
+        if k in valid_keys or k.startswith("user[custom_attributes]")
+    }
+
+    url = os.environ["EZO_BASE_URL"] + "members.api"
+
+    try:
+        response = requests.post(
+            url,
+            headers={"Authorization": "Bearer " + os.environ["EZO_TOKEN"]},
+            data=member,
+            timeout=10,
+        )
+    except Exception as e:
+        print("Error, could not create member in EZOfficeInventory: ", e)
+        raise Exception(
+            "Error, could not create member in EZOfficeInventory: " + str(e)
+        )
+
+    return response.json()
+
+
 def get_locations(filter: Optional[dict]) -> list[dict]:
     """
     Get locations
@@ -148,7 +211,7 @@ def get_locations(filter: Optional[dict]) -> list[dict]:
     all_locations = []
 
     while True:
-        params = {"page": page}
+        params = {"page": page, "include_custom_fields": "true"}
         if filter is not None:
             params.update(filter)
 
@@ -197,8 +260,168 @@ def get_locations(filter: Optional[dict]) -> list[dict]:
     return all_locations
 
 
-# Assets Functions
-# TODO
+def get_location_details(location_num: int) -> dict:
+    """
+    Get location details
+    """
+    if "EZO_BASE_URL" not in os.environ:
+        raise Exception("EZO_BASE_URL not found in environment variables")
+    if "EZO_TOKEN" not in os.environ:
+        raise Exception("EZO_TOKEN not found in environment variables")
+
+    url = os.environ["EZO_BASE_URL"] + "locations/" + str(location_num) + ".api"
+
+    try:
+        response = requests.get(
+            url,
+            headers={"Authorization": "Bearer " + os.environ["EZO_TOKEN"]},
+            params={"include_custom_fields": "true"},
+            timeout=10,
+        )
+    except Exception as e:
+        print("Error, could not get location from EZOfficeInventory: ", e)
+        raise Exception(
+            "Error, could not get location from EZOfficeInventory: " + str(e)
+        )
+
+    if response.status_code != 200:
+        print(
+            f"Error {response.status_code}, could not get location from EZOfficeInventory: ",
+            response.content,
+        )
+        raise Exception(
+            f"Error {response.status_code}, could not get location from EZOfficeInventory: "
+            + str(response.content)
+        )
+
+    return response.json()
+
+
+def create_location(location: dict) -> dict:
+    """
+    Create a location
+    """
+    if "EZO_BASE_URL" not in os.environ:
+        raise Exception("EZO_BASE_URL not found in environment variables")
+    if "EZO_TOKEN" not in os.environ:
+        raise Exception("EZO_TOKEN not found in environment variables")
+
+    # Required fields
+    if "location[name]" not in location:
+        raise ValueError("location must have 'location[name]' key")
+
+    # Remove any keys that are not valid
+    valid_keys = [
+        "location[parent_id]",
+        "location[name]",
+        "location[city]",
+        "location[state]",
+        "location[zipcode]",
+        "location[street1]",
+        "location[street2]",
+        "location[status]",
+        "location[description]",
+    ]
+
+    location = {k: v for k, v in location.items() if k in valid_keys}
+
+    if "location[status]" in location:
+        if location["location[status]"] not in ["active", "inactive"]:
+            raise ValueError(
+                "location['location[status]'] must be one of 'active', 'inactive'"
+            )
+
+    url = os.environ["EZO_BASE_URL"] + "locations.api"
+
+    try:
+        response = requests.post(
+            url,
+            headers={"Authorization": "Bearer " + os.environ["EZO_TOKEN"]},
+            data=location,
+        )
+    except Exception as e:
+        print("Error, could not create location in EZOfficeInventory: ", e)
+        raise Exception(
+            "Error, could not create location in EZOfficeInventory: " + str(e)
+        )
+
+    return response.json()
+
+
+def get_assets(filter: Optional[dict]) -> list[dict]:
+    """
+    Get assets
+    Optionally use filter
+    """
+    if filter is not None:
+        if "status" not in filter:
+            raise ValueError("filter must have 'status' key")
+
+    if "EZO_BASE_URL" not in os.environ:
+        raise Exception("EZO_BASE_URL not found in environment variables.")
+    if "EZO_TOKEN" not in os.environ:
+        raise Exception("EZO_TOKEN not found in environment variables.")
+
+    url = os.environ["EZO_BASE_URL"] + "assets.api"
+
+    page = 1
+    all_assets = []
+
+    while True:
+        params = {"page": page}
+        if filter is not None:
+            params.update(filter)
+
+        try:
+            response = requests.get(
+                url,
+                headers={"Authorization": "Bearer " + os.environ["EZO_TOKEN"]},
+                params=params,
+                data={
+                    "include_custom_fields": "true",
+                    "show_document_urls": "true",
+                    "show_image_urls": "true",
+                },
+                timeout=10,
+            )
+        except Exception as e:
+            print("Error, could not get assets from EZOfficeInventory: ", e)
+            raise Exception(
+                "Error, could not get assets from EZOfficeInventory: " + str(e)
+            )
+
+        if response.status_code != 200:
+            print(
+                f"Error {response.status_code}, could not get assets from EZOfficeInventory: ",
+                response.content,
+            )
+            break
+
+        data = response.json()
+
+        if "assets" not in data:
+            print(
+                f"Error, could not get assets from EZOfficeInventory: ",
+                response.content,
+            )
+            raise Exception(
+                f"Error, could not get assets from EZOfficeInventory: "
+                + str(response.content)
+            )
+
+        all_assets.extend(data["assets"])
+
+        if "total_pages" not in data:
+            print("Error, could not get total_pages from EZOfficeInventory: ", data)
+            break
+
+        if page >= data["total_pages"]:
+            break
+
+        page += 1
+
+    return all_assets
+
 
 if __name__ == "__main__":
     """
@@ -214,8 +437,7 @@ if __name__ == "__main__":
     os.environ["EZO_BASE_URL"] = config["EZO_BASE_URL"]
     os.environ["EZO_TOKEN"] = config["EZO_TOKEN"]
 
-    # members = get_members(
-    #     {"filter": "employee_identification_number", "filter_val": "10431"}
-    # )
-    locations = get_locations({"status": "active"})
+    # Get all assets
+    assets = get_assets(None)
+
     pass
