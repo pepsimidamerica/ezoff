06300b27ace9e5af986e0c2f1613940a77fd6709
Jordan Maynor <mail@jtm.li>
2025-05-22 13:03:13 -0500

    Testing

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,10 @@
-import sys
-
 from dotenv import load_dotenv
 
 # Load env vars from a .env file
 load_dotenv()
 
-sys.path.insert(0, "")
+from ezoff.ezoff import get_teams
 
-from ezoff import get_filtered_assets
-
-res = get_filtered_assets({"status": "possessions_of", "filter_param_val": 244382})
+res = get_teams()
 print(res)
 pass
