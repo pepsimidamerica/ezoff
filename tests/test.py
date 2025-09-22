e044817ffcbc576295833c6cd0b0d3ed52110f66
Jordan Maynor <mail@jtm.li>
2025-09-22 14:22:13 -0500

    Updated dependabot

--- tests/test.py
+++ tests/test.py
@@ -9,5 +9,7 @@ sys.path.insert(0, "")
 
 from ezoff import work_orders_return
 
-res = work_orders_return({"assigned_to_id": 1336290, "state": "Open"})
+res = work_orders_return(
+    {"assigned_to_id": 1336290, "state": ["in_progress", "not_started"]}
+)
 pass
