e9178f1cf1f517687952389e11acc45ebcc1c1e7
Jordan Maynor <mail@jtm.li>
2025-09-25 15:28:57 -0500

    Retire reasons docstrings

--- ezoff/retire_reasons.py
+++ ezoff/retire_reasons.py
@@ -12,6 +12,9 @@ logger = logging.getLogger(__name__)
 def retire_reasons_return() -> list[RetireReason]:
     """
     Returns all retire reasons.
+
+    :return: A list of all retire reasons.
+    :rtype: list[RetireReason]
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/retire_reasons"
