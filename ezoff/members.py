e6ed73a0ed054cf2f53f8bb9fb93cce23bde46b9
Jordan Maynor <mail@jtm.li>
2025-09-25 15:19:56 -0500

    Members docstrings

--- ezoff/members.py
+++ ezoff/members.py
@@ -41,6 +41,51 @@ def member_create(
 ) -> Member | None:
     """
     Create a new member.
+
+    :param first_name: First name of the member
+    :type first_name: str | None
+    :param last_name: Last name of the member
+    :type last_name: str
+    :param role_id: Role ID for the member, corresponds to what permissions they have
+    :type role_id: int
+    :param email: Email address of the member. Note: must be unique.
+    :type email: str
+    :param employee_identification_number: Employee ID number for the member
+    :type employee_identification_number: str, optional
+    :param team_ids: List of team IDs to assign the member to
+    :type team_ids: list[int], optional
+    :param user_listing_id: User listing ID to associate with the member
+    :type user_listing_id: int, optional
+    :param login_enabled: Whether the member can log in. Non-login members can be used for non-employees or simply tracking purposes.
+    :type login_enabled: bool, optional
+    :param subscribed_to_emails: Whether the member is subscribed to emails
+    :type subscribed_to_emails: bool, optional
+    :param skip_confirmation_email: Whether to skip sending a confirmation email
+    :type skip_confirmation_email: bool, optional
+    :param address_name: Name associated with the member's address
+    :type address_name: str, optional
+    :param address: Address of the member
+    :type address: str, optional
+    :param address_line_2: Second line of the member's address
+    :type address_line_2: str, optional
+    :param city: City of the member's address
+    :type city: str, optional
+    :param state: State of the member's address
+    :type state: str, optional
+    :param zip_code: Zip code of the member's address
+    :type zip_code: str, optional
+    :param country: Country of the member's address
+    :type country: str, optional
+    :param fax: Fax number of the member
+    :type fax: str, optional
+    :param phone_number: Phone number of the member
+    :type phone_number: str, optional
+    :param image_url: URL of the member's image
+    :type image_url: str, optional
+    :param custom_fields: List of custom fields for the member
+    :type custom_fields: list[dict], optional
+    :return: The created member, or None if creation failed
+    :rtype: Member | None
     """
 
     params = {k: v for k, v in locals().items() if v is not None}
@@ -78,6 +123,10 @@ def member_create(
 def members_create(members: list[MemberCreate]) -> list[Member] | None:
     """
     Creates new members in bulk.
+
+    :param members: A list of MemberCreate objects representing the members to create. Same fields present in member_create()
+    :return: The list of created Members if successful, else None
+    :rtype: list[Member]
     """
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/members/bulk_create"
 
@@ -115,6 +164,10 @@ def members_create(members: list[MemberCreate]) -> list[Member] | None:
 def member_return(member_id: int) -> Member | None:
     """
     Returns a particular member.
+
+    :param member_id: The ID of the member to return
+    :return: The member if found, else None
+    :rtype: Member | None
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/members/{member_id}"
@@ -157,6 +210,11 @@ def member_return(member_id: int) -> Member | None:
 def members_return(filter: dict | None = None) -> list[Member]:
     """
     Returns all members. Optionally, filter by one or more member fields.
+
+    :param filter:  Dictionary of member fields and the values to filter results by.
+    :type filter: dict, optional
+    :return: List of members
+    :rtype: list[Member]
     """
 
     if filter:
@@ -221,6 +279,11 @@ def members_return(filter: dict | None = None) -> list[Member]:
 def member_update(member_id: int, update_data: dict) -> Member | None:
     """
     Updates a particular member.
+
+    :param member_id: The ID of the member to update
+    :param update_data: Dictionary of fields to update on the member. See Member model for valid fields.
+    :return: The updated member if successful, else None
+    :rtype: Member | None
     """
 
     for field in update_data:
@@ -260,6 +323,10 @@ def member_update(member_id: int, update_data: dict) -> Member | None:
 def member_activate(member_id: int) -> Member | None:
     """
     Activates a particular member.
+
+    :param member_id: The ID of the member to activate
+    :return: The activated member if successful, else None
+    :rtype: Member | None
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/members/{member_id}/activate"
@@ -294,6 +361,10 @@ def member_activate(member_id: int) -> Member | None:
 def member_deactivate(member_id: int) -> Member | None:
     """
     Deactivates a particular member.
+
+    :param member_id: The ID of the member to deactivate
+    :return: The deactivated member if successful, else None
+    :rtype: Member | None
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/members/{member_id}/deactivate"
@@ -327,7 +398,10 @@ def member_deactivate(member_id: int) -> Member | None:
 @Decorators.check_env_vars
 def custom_roles_return() -> list[CustomRole]:
     """
-    Get all custom roles
+    Get all custom roles.
+
+    :return: List of custom roles
+    :rtype: list[CustomRole]
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/custom_roles"
@@ -375,6 +449,11 @@ def custom_roles_return() -> list[CustomRole]:
 def custom_role_update(custom_role_id: int, update_data) -> CustomRole | None:
     """
     Updates a particular custom role.
+
+    :param custom_role_id: The ID of the custom role to update
+    :param update_data: Dictionary of fields to update on the custom role. See CustomRole model for valid fields.
+    :return: The updated custom role if successful, else None
+    :rtype: CustomRole | None
     """
 
     for field in update_data:
@@ -413,7 +492,10 @@ def custom_role_update(custom_role_id: int, update_data) -> CustomRole | None:
 @Decorators.check_env_vars
 def teams_return() -> list[Team]:
     """
-    Get all teams
+    Get all teams.
+
+    :return: List of teams
+    :rtype: list[Team]
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/teams"
@@ -462,7 +544,10 @@ def user_listings_return() -> list[UserListing]:
     """
     Returns all user listings.
     Note: This API endpoint is documented, but I only ever get a 403 when
-    trying to use it. Even though obviously using the API key.
+    trying to use it. Even though obviously using the API key. Not sure.
+
+    :return: List of user listings
+    :rtype: list[UserListing]
     """
 
     url = f"https://{os.environ['EZO_SUBDOMAIN']}.ezofficeinventory.com/api/v2/user_listings"
